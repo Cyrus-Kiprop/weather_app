73a73ced0b59e1bc431833f799ed6ea2292e0052
kip <cyruskiprop254@gmail.com>
2020-07-16 14:41:53 +0300

    finalize on the todays weather section

--- src/components/today/index.js
+++ src/components/today/index.js
@@ -1,7 +1,22 @@
 import utils from '../UxUtils';
+import dateTimeLocation from './dateTimeLocation';
 
 const { make } = utils;
 
 const todayContainer = make('div', 'today__weather-wrapper');
+
+async function database() {
+  const promiseArr = [];
+  ['sys', 'main', 'name', 'weather', 'wind', 'dt'].forEach((key) => {
+    const p = Promise.resolve(JSON.parse(window.localStorage.getItem(key)));
+  });
+
+  return Promise.all(promiseArr).then((result) => {
+    console.log(result);
+  });
+}
+
+database();
+// dateTimeLocation();
 
 export default todayContainer;
